6f4fd928100048c5032ba985102256adef6b4f62
adamiman1807 <adamiman1807@gmail.com>
2024-06-21 01:13:48 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,1 @@
-document.getElementById('myButton').addEventListener('click',function() {
-    var flowerContainer = document.getElementById('flowerContainer');
-    flowerContainer.classList.toggle('visible');
-    flowerContainer.classList.toggle('hidden');
-    alert('YOU ClICKED IT OMGGGG THANKSSSSS !!!!!!!!');
-});
+
